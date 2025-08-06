8bb0bc351cda6d3b17dbb05b2bddb7219af8abe1
Basti <89860334+Xerolux@users.noreply.github.com>
2025-08-06 12:12:39 +0200

    Update install_packages.sh

--- runs/install_packages.sh
+++ runs/install_packages.sh
@@ -2,12 +2,12 @@
 echo "install required packages with 'apt-get'..."
 sudo apt-get -q update
 sudo apt-get -q -y install \
-	vim bc jq socat sshpass sudo ssl-cert mmc-utils \
-	apache2 libapache2-mod-php \
-	php php-gd php-curl php-xml php-json \
-	git \
-	mosquitto mosquitto-clients \
-	python3-pip \
-	xserver-xorg x11-xserver-utils openbox-lxde-session lightdm lightdm-autologin-greeter accountsservice \
-	chromium chromium-l10n
+vim bc jq socat sshpass sudo ssl-cert mmc-utils \
+apache2 libapache2-mod-php \
+php php-gd php-curl php-xml php-json \
+git \
+mosquitto mosquitto-clients \
+python3-pip python3-venv \
+xserver-xorg x11-xserver-utils openbox-lxde-session lightdm lightdm-autologin-greeter accountsservice \
+chromium chromium-l10n
 echo "done"
